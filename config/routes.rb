48a5e24eb64cace2798a8f4e9123b35dfa773db2
Tomas Ibarra <tomas.ibarrab@gmail.com>
2016-11-21 12:16:06 -0700

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'welcome/index'
-  root 'welcome#index'
 
   authenticated :user do
     root 'notes#index', as: 'authenticated_root'
     resources :notes
   end
+  root 'welcome#index'
 end
